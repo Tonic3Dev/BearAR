5a2ba61205748053f3157fdafec37d2d19ca9464
Joao <joao.bittar@w3americas.com>
2023-09-20 13:38:03 -0300

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+window.onload = () => {
+    staticLoadPlaces().then(places => {
+        renderPlaces(places);
+    }).catch(error => {
+        console.error(error);
+    });
+};
+
+function staticLoadPlaces() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                resolve([{
+                    name: 'Bear',
+                    location: {
+                        lat: position.coords.latitude ,
+                        lng: position.coords.longitude,
+                    },
+                }]);
+            }, error => {
+                reject(error);
+            });
+        } else {
+            reject(new Error("Geolocation is not supported by this browser."));
+        }
+        
+    });
+}
+
+var models = [
+    {
+        url: './Assets/bear.glb',
+        scale: '0.1 0.1 0.1',
+        position: '1 1.5 -3.5',
+    },
+];
+
+var modelIndex = 0;
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
+
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+        console.log(model.rotation);
+    }
+
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
+
+    if (model.animation) {
+        entity.setAttribute('animation-mixer', {clip: model.animation});
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let outerEntity = document.createElement('a-entity');
+        console.log(latitude);
+        console.log(longitude);
+        outerEntity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        let modelEntity = document.createElement('a-entity');
+        setModel(models[modelIndex], modelEntity);
+
+        modelEntity.setAttribute('animation-mixer', '');
+        modelEntity.setAttribute("look-at", "[gps-new-camera]");
+
+        outerEntity.appendChild(modelEntity);
+        scene.appendChild(outerEntity);
+    });
+}
